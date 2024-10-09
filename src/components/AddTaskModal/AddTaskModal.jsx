e8f73dad54e3927eb6be82cb2eb91345f14ae68a
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-09 13:14:52 +0100

    wip: add task modal style

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 
 import { v4 as uuidv4 } from 'uuid'
 
+import { IoCloseOutline } from 'react-icons/io5'
+
+import './AddTaskModal.scss'
+
 const AddTaskModal = ({ addTask, checkAddTaskShowModal }) => {
   const [showTitleInputError, setShowTitleInputError] = useState(false)
 
@@ -37,13 +41,18 @@ const AddTaskModal = ({ addTask, checkAddTaskShowModal }) => {
   return (
     <div className='to-do-list--add-task-modal'>
       <div className='to-do-list--add-task-modal-body'>
-        <button className='to-do-list--add-task-close-button' onClick={handleCloseModalClick}>X</button>
-        <h2 className='to-do-list--add-task-modal-title'>New task</h2>
+        <button className='to-do-list--add-task-close-button' onClick={handleCloseModalClick}>
+          <IoCloseOutline />
+        </button>
+        <h2 className='to-do-list--add-task-modal-title'>New Task</h2>
         <form className='to-do-list--add-task-form' onSubmit={handleSubmit}>
-          <input type='text' name='title' placeholder='Enter a title' className='to-do-list--add-task-input' />
+          <input type='text' name='title' placeholder='Enter a title...' className='to-do-list--form-field to-do-list--add-task-input' />
           {showTitleInputError && <p className='to-do-list--add-task-input-error'>Please enter a title</p>}
-          <textarea name='description' placeholder='Enter a description' className='to-do-list--add-task-textarea' />
-          <button type='submit' className='to-do-list--add-task-submit-button'>Add task</button>
+          <textarea name='description' placeholder='Enter a description...' className='to-do-list--form-field to-do-list--add-task-textarea' />
+          <div className='to-do-list--add-task-form-actions'>
+            <button className='to-do-list--add-task-cancel-button'>Cancel</button>
+            <button type='submit' className='to-do-list--add-task-submit-button'>Add Task</button>
+          </div>
         </form>
       </div>
     </div>
