33d4aa96b009dd845bec95e8736b106e83d8a4a2
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-10 15:59:35 +0100

    fix: about title input error message in add task modal

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -7,6 +7,7 @@ import { IoCloseOutline } from 'react-icons/io5'
 import '../../style/TaskFormModal.scss'
 
 const AddTaskModal = ({ addTask, checkAddTaskShowModal }) => {
+  const [titleInput, setTitleInput] = useState('')
   const [showTitleInputError, setShowTitleInputError] = useState(false)
 
   const handleCloseModalClick = () => {
@@ -38,6 +39,18 @@ const AddTaskModal = ({ addTask, checkAddTaskShowModal }) => {
     checkAddTaskShowModal(false)
   }
 
+  const handleTitleChange = (event) => {
+    const { value } = event.target
+
+    setTitleInput(value)
+
+    if (!value) {
+      setShowTitleInputError(true)
+    } else {
+      setShowTitleInputError(false)
+    }
+  }
+
   return (
     <div className='to-do-list--task-modal'>
       <div className='to-do-list--task-modal-body'>
@@ -46,7 +59,7 @@ const AddTaskModal = ({ addTask, checkAddTaskShowModal }) => {
         </button>
         <h2 className='to-do-list--task-modal-title'>New Task</h2>
         <form className='to-do-list--task-form' onSubmit={handleSubmit}>
-          <input type='text' name='title' placeholder='Enter a title...' className='to-do-list--form-field to-do-list--task-input' />
+          <input type='text' value={titleInput} name='title' placeholder='Enter a title...' className='to-do-list--form-field to-do-list--task-input' onChange={handleTitleChange} />
           {showTitleInputError && <p className='to-do-list--task-input-error'>Please enter a title</p>}
           <textarea name='description' placeholder='Enter a description...' className='to-do-list--form-field to-do-list--task-textarea' />
           <div className='to-do-list--task-form-actions'>
