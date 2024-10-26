edc82995d7f7afb29e22142cceac3b0e1f9084b5
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-26 14:40:27 +0100

    fix: reset Task Form Fields when open or close add task modal

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -8,10 +8,24 @@ import '../../style/TaskFormModal.scss'
 
 const AddTaskModal = ({ addTask, isAddTaskModalOpen, onAddTaskModalClose }) => {
   const dialogRef = useRef(null)
-  const [titleInput, setTitleInput] = useState('')
+  const [taskForm, setTaskForm] = useState({
+    taskTitle: '',
+    taskDescription: ''
+  })
   const [showTitleInputError, setShowTitleInputError] = useState(false)
 
   useEffect(() => {
+    const resetTaskFormFields = () => {
+      const newTaskForm = {
+        taskTitle: '',
+        taskDescription: ''
+      }
+
+      setTaskForm(newTaskForm)
+    }
+
+    resetTaskFormFields()
+
     if (isAddTaskModalOpen) {
       dialogRef.current?.showModal()
       document.body.classList.add('no-scroll')
@@ -21,13 +35,17 @@ const AddTaskModal = ({ addTask, isAddTaskModalOpen, onAddTaskModalClose }) => {
     }
   }, [isAddTaskModalOpen])
 
+  // WIP: FIX ABOUT THIS
+  /*
   const handleDialogClick = (event) => {
-    const { target } = event
+    const { target, currentTarget } = event
 
-    if (target === dialogRef.current) {
+    console.log(target, currentTarget)
+
+    if (target === dialogRef.current && target === currentTarget) {
       onAddTaskModalClose()
     }
-  }
+  } */
 
   const handleCloseModal = () => {
     onAddTaskModalClose()
@@ -60,8 +78,12 @@ const AddTaskModal = ({ addTask, isAddTaskModalOpen, onAddTaskModalClose }) => {
 
   const handleTitleChange = (event) => {
     const { value } = event.target
+    const newTasForm = {
+      ...taskForm,
+      taskTitle: value
+    }
 
-    setTitleInput(value)
+    setTaskForm(newTasForm)
 
     if (!value) {
       setShowTitleInputError(true)
@@ -70,16 +92,26 @@ const AddTaskModal = ({ addTask, isAddTaskModalOpen, onAddTaskModalClose }) => {
     }
   }
 
+  const handleDescriptionChange = (event) => {
+    const { value } = event.target
+    const newTaskForm = {
+      ...taskForm,
+      taskDescription: value
+    }
+
+    setTaskForm(newTaskForm)
+  }
+
   return (
-    <dialog className='to-do-list--task-modal' ref={dialogRef} onClick={handleDialogClick} onClose={handleCloseModal}>
+    <dialog className='to-do-list--task-modal' ref={dialogRef} onClose={handleCloseModal}>
       <button className='to-do-list--task-close-button' autoFocus onClick={handleCloseModal}>
         <IoCloseOutline />
       </button>
       <h2 className='to-do-list--task-modal-title'>New Task</h2>
       <form className='to-do-list--task-form' onSubmit={handleSubmit}>
-        <input type='text' value={titleInput} name='title' placeholder='Enter a title...' className='to-do-list--form-field to-do-list--task-input' onChange={handleTitleChange} />
+        <input type='text' value={taskForm.taskTitle} name='title' placeholder='Enter a title...' className='to-do-list--form-field to-do-list--task-input' onChange={handleTitleChange} />
         {showTitleInputError && <p className='to-do-list--task-input-error'>Please enter a title</p>}
-        <textarea name='description' placeholder='Enter a description...' className='to-do-list--form-field to-do-list--task-textarea' />
+        <textarea value={taskForm.taskDescription} name='description' placeholder='Enter a description...' className='to-do-list--form-field to-do-list--task-textarea' onChange={handleDescriptionChange} />
         <div className='to-do-list--task-form-actions'>
           <button className='to-do-list--task-cancel-button' onClick={handleCloseModal}>Cancel</button>
           <button type='submit' className='to-do-list--task-submit-button'>Add Task</button>
