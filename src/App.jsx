1b30f080575746be1d2fd1e4b9699cdaced00a3a
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-10-31 09:38:41 +0000

    refactor: change header element to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.scss'
+import Header from './components/Header/Header'
 import Dashboard from './components/Dashboard/Dashboard'
 import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
     <div className='to-do-list-app'>
-      <header className='header'>
-        <h1 className='header__main-title'>To Do List</h1>
-      </header>
+      <Header mainTitle='To Do List' />
       <Dashboard />
       <Footer />
     </div>
