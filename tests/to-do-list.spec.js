e8c38e157b4cfbb66438d4ad88025362b1996726
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-11-06 10:55:48 +0000

    test: about showing a required message when a task's title is empty

--- tests/to-do-list.spec.js
+++ tests/to-do-list.spec.js
@@ -5,10 +5,20 @@ import { storageGetTasks } from '../src/storage'
 
 const LOCALHOST_URL = 'http://localhost:5173/'
 
-test('should show a message when there are no tasks', async ({ page }) => {
+const tasks = [{
+  title: 'Go to the supermarket',
+  description: 'Buy milk and eggs'
+}, {
+  title: 'Go to the post office',
+  description: 'Package for John'
+}]
+
+test.beforeEach(async ({ page }) => {
   await page.exposeFunction('storageGetTasks', storageGetTasks)
   await page.goto(LOCALHOST_URL)
+})
 
+test('should show a message when there are no tasks', async ({ page }) => {
   const storageTasks = await page.evaluate(async () => {
     const tasks = await window.storageGetTasks()
 
@@ -23,4 +33,22 @@ test('should show a message when there are no tasks', async ({ page }) => {
     await expect(messageText?.length).toBeGreaterThan(0)
     await expect(messageText).toContain('There are no tasks')
   }
+})
+
+test.describe('New task', () => {
+  test("should show a required message when a task's title is empty", async ({ page }) => {
+    const addTaskButton = await page.locator('.add-task-button')
+    await addTaskButton.click()
+
+    const modalAddTaskButton = await page.locator('.main__task-dialog .form-action--submit-button')
+    await modalAddTaskButton.click()
+
+    const formFieldTitleElem = await page.locator('.main__task-dialog .form__field--title')
+    const formFieldTitleValue = await formFieldTitleElem.inputValue()
+
+    if (!formFieldTitleValue) {
+      const titleErrorElem = await page.locator('.form__title-error')
+      await expect(titleErrorElem).toBeVisible()
+    }
+  })
 })
