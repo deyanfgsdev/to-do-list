d29f0190e0d34ddeb4f306ddca392e9c09ca1d2f
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-10 12:55:30 +0000

    refactor: add SocialMedia type

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+/* Social Media */
+
+import { IconType } from 'react-icons';
+
+interface SocialMedia {
+  id: number;
+  icon: IconType;
+  url: string;
+}
+
+export type SocialMediaList = SocialMedia[];
