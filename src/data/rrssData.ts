6f853be7e19cc631f49b6f9909c3ec2d366662ec
Deyán F. Guacarán Sabogal <deyan.fgs904@gmail.com>
2024-12-10 12:56:50 +0000

    refactor: convert rrssData from JS to TS

---
+++ src/data/rrssData.ts
@@ -0,0 +1,24 @@
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+
+import { SocialMediaList } from '../types/global';
+
+const rrssData: SocialMediaList = [
+  {
+    id: 1,
+    icon: FaGithub,
+    url: 'https://github.com/deyanfgsdev',
+  },
+  {
+    id: 2,
+    icon: FaLinkedinIn,
+    url: 'https://www.linkedin.com/in/deyanfgsdev/',
+  },
+  {
+    id: 3,
+    icon: FaXTwitter,
+    url: 'https://twitter.com/deyanfgs',
+  },
+];
+
+export default rrssData;
